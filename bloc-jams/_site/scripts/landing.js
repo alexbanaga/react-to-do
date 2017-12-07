1f3dc7b3d6e067bcd5c782141708ab78bcf935bb
alexbanaga <alexbanaga619@gmail.com>
2017-12-07 01:36:33 -0800

    add delete buttons to list items

---
+++ bloc-jams/_site/scripts/landing.js
@@ -0,0 +1,34 @@
+var animatePoints = document.getElementsByClassName('point');
+
+var revealPoint = function(point) {
+  point.style.opacity = 1;
+  point.style.transform = "scaleX(1) translateY(0)";
+  point.style.msTransform = "scaleX(1) translateY(0)";
+  point.style.WebkitTransform = "scaleX(1) translateY(0)";
+};
+
+var animatePoints = function() {
+  var revealPoint = function() {
+    $(this).css({
+      opacity: 1,
+      transform: 'scaleX(1) translateY(0)'
+    });
+  };
+  $.each($('.point'), revealPoint);
+};
+
+$(window).load(function() {
+
+  if ($(window).height() > 950) {
+    animatePoints();
+  }
+
+  var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+
+  $(window).scroll(function(event) {
+    if ($(window).scrollTop() >= scrollDistance) {
+      animatePoints();
+    };
+  });
+
+});
--- bloc-jams/scripts/landing.js
+++ bloc-jams/scripts/landing.js
@@ -1,16 +1,34 @@
-var animatePoints = function() {
-   var points  = document.getElementsByClassName('point');
+var animatePoints = document.getElementsByClassName('point');
 
-   var revealPoint = function(index) {
-     points[index].style.opacity = 1;
-     points[index].style.transform = "scaleX(1) translateY(0)";
-     points[index].style.msTransform = "scaleX(1) translateY(0)";
-     points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-   }
-
-    for (var i = 0; i < points.length; i++) {
-      revealPoint(i);
-    }
+var revealPoint = function(point) {
+  point.style.opacity = 1;
+  point.style.transform = "scaleX(1) translateY(0)";
+  point.style.msTransform = "scaleX(1) translateY(0)";
+  point.style.WebkitTransform = "scaleX(1) translateY(0)";
 };
 
-animatePoints();
+var animatePoints = function() {
+  var revealPoint = function() {
+    $(this).css({
+      opacity: 1,
+      transform: 'scaleX(1) translateY(0)'
+    });
+  };
+  $.each($('.point'), revealPoint);
+};
+
+$(window).load(function() {
+
+  if ($(window).height() > 950) {
+    animatePoints();
+  }
+
+  var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+
+  $(window).scroll(function(event) {
+    if ($(window).scrollTop() >= scrollDistance) {
+      animatePoints();
+    };
+  });
+
+});
